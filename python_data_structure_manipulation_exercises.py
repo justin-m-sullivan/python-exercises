cbee11c38d3ade93d5f911f1771723b14c267271
Justin Sullivan <sullivan.justin.michael@gmail.com>
2021-01-14 17:06:23 -0600

    updated code for exercises after demo

---
+++ python_data_structure_manipulation_exercises.py
@@ -0,0 +1,25 @@
+## Python Data Structues and manipulation exercises
+# The following questions reference the students data structure below. Write the python code to answer the following questions:
+
+1. How many students are there? -- 14
+len(students)
+
+2. How many students prefer light coffee? For each type of coffee roast?
+3. How many types of each pet are there?
+4. How many grades does each student have? Do they all have the same number of grades?
+5. What is each student's grade average?
+6. How many pets does each student have?
+7. How many students are in web development? data science?
+8. What is the average number of pets for students in web development?
+9. What is the average pet age for students in data science?
+10. What is most frequent coffee preference for data science students?
+11. What is the least frequent coffee preference for web development students?
+12. What is the average grade for students with at least 2 pets?
+13. How many students have 3 pets?
+14. What is the average grade for students with 0 pets?
+15. What is the average grade for web development students? data science students?
+16. What is the average grade range (i.e. highest grade - lowest grade) for dark coffee drinkers?
+17. What is the average number of pets for medium coffee drinkers?
+18. What is the most common type of pet for web development students?
+19. What is the average name length?
+20. What is the highest pet age for light coffee drinkers?3. 
